885ddc8162eb95db2d18582deb7d575bfe8d0205
Admin01 <ismaail_89@yahoo.co.uk>
2020-10-31 13:39:55 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,19 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running....')
 })
 
-app.use('api/products', productRoutes)
+app.use('/api/products', productRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
